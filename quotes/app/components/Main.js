75b33f2838e3d6112107cd42925687dcb83d335c
rukaroa <rukaroa@gmail.com>
2017-06-08 21:31:30 -0500

    despair

---
+++ quotes/app/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import Quote from "./Quote";
+// import Input from "./Input";
+
+// very basic component to get started
+const Main = () => (
+  <div className="container">
+    <div className="jumbotron">
+        <h1>TEST</h1>
+    </div>
+  </div>
+);
+
+export default Main;
