05a3d760b862cb1f66e958ce12a7230dffea76f6
rukaroa <rukaroa@gmail.com>
2017-06-08 20:08:24 -0500

    concats

---
+++ JavaScript_Spread_Syntax.js
@@ -0,0 +1,14 @@
+// Array Literals 
+var parts = ['shoulders', 'knees']; 
+var lyrics = ['head', ...parts, 'and', 'toes']; 
+// ["head", "shoulders", "knees", "and", "toes"]
+
+// Object Literals
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 13 };
+
+var clonedObj = { ...obj1 };
+// Object { foo: "bar", x: 42 }
+
+var mergedObj = { ...obj1, ...obj2 };
+// Object { foo: "baz", x: 42, y: 13 }
